c25cfb2f056468dc809817a3bc07c6eab5079e55
hiencow <nguyenvanhien18072006@gmail.com>
2026-04-28 20:40:25 +0700

    Phần 2: Lớp, Đối tượng và Biến

--- QuanLyDeTu/Program.cs
+++ QuanLyDeTu/Program.cs
@@ -1,1 +1,18 @@
-﻿// Khoi tao du an QuanLyDeTu
+﻿// Phan 1
+// Khoi tao du an QuanLyDeTu
+ using System;
+ namespace QuanLyDeTu
+ {
+    class Program
+    {
+        static void Main()
+        {
+            DeTu dt = new DeTu();
+            dt.NhapThongTin();
+            dt.HienThi();
+        }
+        
+    }
+ }
+
+ 
